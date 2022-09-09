f83ef93f235e3f6c094819d15c656e8e350e65ea
adhfmz7 <ahmadaldasouqi559@gmail.com>
2022-09-09 03:23:25 -0700

    fixed deprecated selenium features

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ for i in range(25):
     grid = collect()
     result = solver(grid)
     fill(result)
-    driver.find_element_by_name('submit').click()
+    driver.find_element(By.NAME, 'submit').click()
