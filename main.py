995d0b7d59bc81999ca11b1c2bf7d958fb7aeab1
adhfmz7 <ahmadaldasouqi559@gmail.com>
2022-09-09 03:41:48 -0700

    made fill in place

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 #!/opt/anaconda3/envs/sudoku/bin/python3
 from sudoku.board import *
 
-for i in range(25):
-    grid = collect()
-    result = solver(grid)
-    fill(result)
-    driver.find_element(By.NAME, 'submit').click()
+board = Board()
+
+for i in range(15):
+    board.solve_puzzle()
